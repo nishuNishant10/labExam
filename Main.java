0a35bc8d9e52e5c6f7cc51b13ae40f613983d6f8
sangramsn123 <nipane.sangram123@gmail.com>
2023-07-25 08:36:58 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = input.nextInt();
+
+        int sum = 0;
+        int max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+
+        if (num3 > max) {
+            max = num3;
+        }
+
+        sum = max;
+
+        System.out.println("The sum of the largest number is: " + sum);
+    }
+}
